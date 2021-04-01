d9abd157a430ba983a54fd159c1d4dee8e9a3297
AmirSalakhov94 <salakhov.amir.94@mail.ru>
2021-04-01 12:00:08 +0300

    fixed #1

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -5,22 +5,9 @@ import tech.itpark.proggerhub.repository.UserRepository;
 import tech.itpark.proggerhub.service.UserService;
 
 public class Main {
-    // psvm + Tab -> main
     public static void main(String[] args) {
-        // TODO: MVP -> Minimal Valuable Product
-        // videos
-        // users/owners <-> consumers
-        // Domain Model -> предметная область
-
-        // 0. Plain Java (manual)
-        // 1. Dependency Lookup -> Service Registry
-        // 2. Dependency Injection -> Container:
-        // 2.1 Сам делал new
-        // 2.2 Найти нужные зависимости, если они требуются для new
         Container container = new Container();
         container.register(UserRepository.class, UserService.class);
         container.wire();
-
-        System.out.println("dfdf");
     }
 }
--- src/main/java/tech/itpark/proggerhub/model/Video.java
+++ src/main/java/tech/itpark/proggerhub/model/Video.java
@@ -2,11 +2,9 @@ package tech.itpark.proggerhub.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Value;
 
-// Data Class
 @AllArgsConstructor
-@Data // get/set + equals/hashCode/toString
+@Data
 public class Video {
     private String id;
     private String name;
